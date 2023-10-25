01bbdefbacd25b51d7b43bc8837dd203962dabb4
user <mehulprajapati1661@gmail.com>
2023-10-25 22:12:23 +0530

    SplashActivity completed

---
+++ app/src/main/java/com/example/mad_assignment_2_21012011118/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.example.mad_assignment_2_21012011118
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finish()
+        }, 3000)
+    }
+}
